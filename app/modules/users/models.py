3429373c3fe02a20982093b91d684db32e6aabff
Wasit Mirani <44178287+wasitmirani@users.noreply.github.com>
2026-09-04 12:06:07 -0700

    feat: add user model with fields for email, password, personal information, and status

--- app/modules/users/models.py
+++ app/modules/users/models.py
@@ -1,4 +1,17 @@
 from app.modules.base_model import BaseModel
+from sqlalchemy import Column, String
 
 class User(BaseModel):
     __tablename__ ="users"
+
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    phone = Column(String, nullable=False)
+    address = Column(String, nullable=False)
+    city = Column(String, nullable=False)
+    state = Column(String, nullable=False)
+    zip_code = Column(String, nullable=False)
+    country = Column(String, nullable=False)
+    status = Column(String, nullable=False)
